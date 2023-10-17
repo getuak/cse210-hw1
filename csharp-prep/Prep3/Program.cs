89269c73331b9ef9d82a6eaa31ccd4f9c83c4842
getuak <ken17002@byui.edu>
2023-10-17 00:21:23 -0600

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate random number between 1 and 101
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+// initialize guess
+        int guess = 0;
+        // while do loop, loops through untill the conditon is met.
+        while (guess != magicNumber){
+
+        
+        //Console.Write("What is the magic number? ");
+        //int response = Console.ReadLine();
+        //Console.Write(response);
+// prompt the user to input the guess and displays the response
+        Console.Write("What is your guess? ");
+        guess = int.Parse( Console.ReadLine());
+
+        if (guess == magicNumber)
+        {
+            Console.Write("Good Job, You got it");
+        }
+        else if (guess > magicNumber){
+            Console.Write("Lower");
+        }
+        else{
+            Console.Write("Higher");
+        }
+
+
+        
+
+        }
     }
 }
